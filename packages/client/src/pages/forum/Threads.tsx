a24639db91c055cce04985752d2f7b4e3f19ab2a
fasty86 <oleg.tolm1986@gmail.com>
2025-03-04 21:12:38 +0300

    feat: добавление страницы с перечнем тем

---
+++ packages/client/src/pages/forum/Threads.tsx
@@ -0,0 +1,120 @@
+import { MessageOutlined } from '@ant-design/icons'
+import { List, Avatar, Typography, Flex, Button, Tag } from 'antd'
+import { useMemo, useState } from 'react'
+import { Link } from 'react-router-dom'
+import ForumNavigation from './components/ForumNavigation'
+import { useAppSelector } from '../../store/hooks/deriveTypes'
+import styles from './/styles/thread.module.css'
+
+import { Thread } from '../../store/features/forum/types'
+import CreateThreadModal from './components/CreateThreadModal'
+import { IconText } from './components/IconText'
+
+function Threads() {
+  const { categories, threads } = useAppSelector(state => state.forum)
+  const [categoryFilter, setcategoryFilter] = useState<number[]>([])
+  const [search, setSearch] = useState<string>('')
+  const [showModal, setShowModal] = useState(false)
+  const filterThreads = useMemo(() => {
+    let result: Thread[] = [...threads]
+    if (categoryFilter.length)
+      result = [...threads].filter(thread =>
+        categoryFilter.includes(thread.categoryId)
+      )
+    if (search !== '') {
+      const searchRegex = new RegExp(search, 'i')
+      result = [...result].filter(thread =>
+        searchRegex.test(thread.description)
+      )
+    }
+    return result
+  }, [categoryFilter, search])
+  return (
+    <Flex vertical gap="large">
+      <ForumNavigation
+        categories={categories}
+        handleSearch={setSearch}
+        handlerCategory={setcategoryFilter}
+      />
+      <List
+        itemLayout="vertical"
+        size="large"
+        dataSource={filterThreads}
+        renderItem={item => (
+          <List.Item
+            onClick={() => console.log(item.id)}
+            className={styles['thread-list-item']}
+            key={item.id}
+            actions={[
+              <div className={styles['thread-list-item-actions']}>
+                <IconText
+                  icon={MessageOutlined}
+                  text={String(item.posts.length)}
+                  key="list-vertical-message"
+                />
+                <Tag
+                  color={
+                    item.categoryId === 3
+                      ? 'volcano'
+                      : item.categoryId === 4
+                      ? 'gold'
+                      : item.categoryId === 2
+                      ? 'processing'
+                      : 'cyan'
+                  }>
+                  {categories.find(cat => cat.id === item.categoryId)?.title}
+                </Tag>
+              </div>,
+            ]}>
+            <List.Item.Meta
+              avatar={<Avatar src={item.authorId} />}
+              title={
+                <Typography.Text style={{ fontSize: '1.7rem' }}>
+                  <Link
+                    style={{ color: '#8076a3' }}
+                    to={{
+                      pathname: `/forum/${item.id}`,
+                    }}>
+                    {item.title}
+                  </Link>
+                </Typography.Text>
+              }
+              description={
+                <Typography.Paragraph
+                  style={{ fontSize: '1rem' }}
+                  ellipsis={{
+                    rows: 1,
+                    expandable: 'collapsible',
+                    symbol: expanded => (expanded ? 'свернуть' : 'развернуть'),
+                  }}>
+                  {item.description}
+                </Typography.Paragraph>
+              }
+            />
+          </List.Item>
+        )}
+      />
+      <Button
+        className={styles['pulsing-button']}
+        color="primary"
+        variant="outlined"
+        size="large"
+        style={{
+          bottom: '30px',
+          position: 'sticky',
+          maxWidth: '400px',
+          alignSelf: 'center',
+        }}
+        onClick={() => setShowModal(curr => !curr)}>
+        добавить тему
+      </Button>
+      <CreateThreadModal
+        isOpen={showModal}
+        closeModal={() => setShowModal(false)}
+        categories={categories}
+      />
+    </Flex>
+  )
+}
+
+export default Threads
---
+++ packages/client/src/pages/forum/components/ForumNavigation.tsx
@@ -0,0 +1,61 @@
+import { FC } from 'react'
+import { Category } from '../../../store/features/forum/types'
+import { Flex, Select } from 'antd'
+import { DefaultOptionType } from 'antd/es/select'
+import Search from 'antd/es/input/Search'
+
+type navigationProps = {
+  categories: Category[]
+  handlerCategory: (categoryID: number[]) => void
+  handleSearch: (text: string) => void
+}
+function prepareOptions(categories: Category[]): DefaultOptionType[] {
+  const options = categories.map((category): DefaultOptionType => {
+    return {
+      label: category.title,
+      value: String(category.id),
+    }
+  })
+  return options
+}
+const ForumNavigation: FC<navigationProps> = ({
+  categories,
+  handleSearch,
+  handlerCategory,
+}) => {
+  const options = prepareOptions(categories)
+  return (
+    <Flex
+      style={{
+        height: '3rem',
+        paddingInline: '1rem',
+        position: 'sticky',
+        top: 0,
+        backgroundColor: '#ffffff',
+        zIndex: 1,
+        marginBottom: '1rem',
+        borderBottom: '1px solid #8076a3',
+      }}
+      justify="space-between"
+      align="center">
+      <Select
+        mode="tags"
+        placeholder="Выбор желаемых категорий"
+        style={{ minWidth: '30%', height: ' 2rem' }}
+        onChange={values => handlerCategory([...values].map(id => Number(id)))}
+        tokenSeparators={[',']}
+        options={options}
+      />
+
+      <Search
+        placeholder="поиск по тексу в описании темы"
+        allowClear
+        // onChange={event => handleSearch(event.target.value)}
+        onSearch={handleSearch}
+        style={{ width: 300 }}
+      />
+    </Flex>
+  )
+}
+
+export default ForumNavigation
