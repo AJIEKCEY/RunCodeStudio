34579c1843d67c7397c2b9c04650587231b02c04
fasty86 <oleg.tolm1986@gmail.com>
2025-03-04 21:10:14 +0300

    feat: изменения в базовом слое

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -3,16 +3,16 @@ import { BrowserRouter } from 'react-router-dom'
 import AppRouter from './AppRoutes'
 
 function App() {
-  useEffect(() => {
-    const fetchServerData = async () => {
-      const url = `http://localhost:${__SERVER_PORT__}`
-      const response = await fetch(url)
-      const data = await response.json()
-      console.log(data)
-    }
+  // useEffect(() => {
+  //   const fetchServerData = async () => {
+  //     const url = `http://localhost:${__SERVER_PORT__}`
+  //     const response = await fetch(url)
+  //     const data = await response.json()
+  //     console.log(data)
+  //   }
 
-    fetchServerData()
-  }, [])
+  //   fetchServerData()
+  // }, [])
   return (
     <BrowserRouter future={{ v7_startTransition: true }}>
       <AppRouter />
--- packages/client/src/components/Layout.tsx
+++ packages/client/src/components/Layout.tsx
@@ -1,16 +1,28 @@
-import { Layout, theme } from 'antd'
+import { Layout, MenuProps, theme } from 'antd'
 import { Content } from 'antd/es/layout/layout'
 
 import LayoutHeader from './LayoutHeader'
 import LayoutFooter from './LayoutFooter'
-import { Outlet } from 'react-router-dom'
+import { Link, Outlet } from 'react-router-dom'
+import styles from './layout.module.css'
 
+<<<<<<< HEAD
+=======
+type MenuItem = Required<MenuProps>['items'][number]
+const items: MenuItem[] = [
+  {
+    key: 'main',
+    label: <Link to="/">На главную</Link>,
+  },
+]
+>>>>>>> f8b1bfa (feat: изменения в базовом слое)
 function Container() {
   const {
     token: { colorBgContainer, borderRadiusLG },
   } = theme.useToken()
 
   return (
+<<<<<<< HEAD
     <Layout
       style={{
         height: '100vh',
@@ -31,6 +43,32 @@ function Container() {
       </Content>
       <LayoutFooter text="RunCode Studio" />
     </Layout>
+=======
+    <>
+      <Layout
+        style={{
+          height: '100vh',
+          paddingTop: 0,
+          maxWidth: '1280px',
+          marginInline: 'auto',
+        }}>
+        <LayoutHeader items={items} />
+        <Content
+          className={styles['layout-container']}
+          style={{ padding: '0 10px', overflowY: 'auto' }}>
+          <div
+            style={{
+              background: colorBgContainer,
+              minHeight: '100%',
+              borderRadius: borderRadiusLG,
+            }}>
+            <Outlet />
+          </div>
+        </Content>
+        <LayoutFooter text="RunCode Studio" />
+      </Layout>
+    </>
+>>>>>>> f8b1bfa (feat: изменения в базовом слое)
   )
 }
 
