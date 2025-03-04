4c7f282e66bc419da3ce10fc7a74d384b2a136a1
fasty86 <oleg.tolm1986@gmail.com>
2025-03-04 21:11:03 +0300

    feat: заполнение слайса по форуму моковыми данными

---
+++ packages/client/src/store/features/forum/forumSlice.ts
@@ -0,0 +1,27 @@
+import { createSlice } from '@reduxjs/toolkit'
+import type { PayloadAction } from '@reduxjs/toolkit'
+import type { RootState } from '../../store'
+import { ForumState, Thread } from './types'
+import { categories, threads, posts } from './mockData'
+
+const initialState: ForumState = {
+  threads,
+  posts,
+  categories,
+}
+
+export const forumSlice = createSlice({
+  name: 'forum',
+  initialState,
+  reducers: {
+    addNewThread: (state, action: PayloadAction<{ thread: Thread }>) => {
+      state.threads = [...state.threads, action.payload.thread]
+    },
+  },
+})
+
+export const { addNewThread } = forumSlice.actions
+
+export const getForumData = (state: RootState) => state.forum
+
+export default forumSlice.reducer
--- packages/client/src/store/store.ts
+++ packages/client/src/store/store.ts
@@ -1,5 +1,5 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit'
-import forumSlice from './features/forumSlice'
+import forumSlice from './features/forum/forumSlice'
 const rootReducer = combineReducers({
   forum: forumSlice,
 })
