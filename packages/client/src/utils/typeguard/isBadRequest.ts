4dcc24cc663d46919a82798a763713e5220f6181
fasty86 <oleg.tolm1986@gmail.com>
2025-03-17 18:04:04 +0300

    feat: изменения в typeguards

--- packages/client/src/utils/typeguard/isBadRequest.ts
+++ packages/client/src/utils/typeguard/isBadRequest.ts
@@ -1,8 +1,9 @@
 import { BadRequest } from '../../store/features/user/types'
+import { ResponseWithStatusField } from './types'
 
 export function isBadRequest(obj: object): obj is BadRequest {
-  if (typeof obj === 'object' && 'status' in obj) {
-    return obj.status !== 200
+  if (typeof obj === 'object' && obj !== null) {
+    if ('status' in obj) return (obj as ResponseWithStatusField).status !== 200
   }
   return false
 }
