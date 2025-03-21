306f08425b4f45b7af92adf60ec7f0f14a82890f
Laura0120 <gabrielyanlaura@gmail.com>
2025-03-21 17:07:41 +0300

    добавлено Fullscreen API

--- packages/client/src/components/Layout.tsx
+++ packages/client/src/components/Layout.tsx
@@ -5,11 +5,30 @@ import LayoutHeader from './LayoutHeader/LayoutHeader'
 import LayoutFooter from './LayoutFooter'
 import { Outlet } from 'react-router-dom'
 import styles from './layout.module.css'
+import { useEffect, useRef } from 'react'
 
 function Container() {
   const {
     token: { colorBgContainer, borderRadiusLG },
   } = theme.useToken()
+  const container = useRef<HTMLElement | null>(null)
+
+  useEffect(() => {
+    const onFullScreen = (e: KeyboardEvent) => {
+      if (e.code === 'KeyF') {
+        if (document.fullscreenElement) {
+          document.exitFullscreen()
+          return
+        }
+        container.current
+          ?.requestFullscreen()
+          .catch(err => console.log(`Ошибка ${err.message}`))
+      }
+    }
+
+    document.addEventListener('keydown', onFullScreen)
+    return () => document.removeEventListener('keydown', onFullScreen)
+  }, [])
 
   return (
     <Layout
@@ -21,7 +40,8 @@ function Container() {
         marginInline: 'auto',
         borderRadius: 0,
         overflow: 'hidden',
-      }}>
+      }}
+      ref={container}>
       <LayoutHeader />
       <Content
         className={styles['layout-container']}
