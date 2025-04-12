14447f47d35ee33433df967bf38980cccc337004
fasty86 <oleg.tolm1986@gmail.com>
2025-04-12 21:50:49 +0300

    refactor: форматирование даты переписано на реализацию через Intl

--- packages/client/src/utils/getFormattedCurrentDate.ts
+++ packages/client/src/utils/getFormattedCurrentDate.ts
@@ -1,6 +1,9 @@
 export function formatDate(date: Date): string {
-  const year = date.getFullYear()
-  const month = String(date.getMonth() + 1).padStart(2, '0')
-  const day = String(date.getDate()).padStart(2, '0')
-  return `${year}-${month}-${day}`
+  const userLocale = navigator.language || 'en-US'
+  const formatter = new Intl.DateTimeFormat(userLocale, {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+  })
+  return formatter.format(date)
 }
