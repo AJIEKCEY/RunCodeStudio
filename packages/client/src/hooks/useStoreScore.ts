179dd92d9d944ef347710ab01fba33ee6a0a5db1
fasty86 <oleg.tolm1986@gmail.com>
2025-05-13 15:32:20 +0300

    feat:  добавлены хуки для определения геолокация пользователя, добавлен хук для записи страны пользователя  при записи результатов в лидерборд

---
+++ packages/client/src/hooks/useStoreScore.ts
@@ -0,0 +1,34 @@
+import { useEffect, useState } from 'react'
+import { useUpdateUserScoreMutation } from '../store/features/leaderboard/leaderBoardApiSlice'
+import { useGetUserQuery } from '../store/features/user/userApiSlice'
+import { useGeolocation } from './usegeolocation'
+import { formatDate } from '../utils/getFormattedCurrentDate'
+
+export const useStoreScore = () => {
+  const [score, setScore] = useState<number | null>(null)
+  const { coordinates, isFetching, country } = useGeolocation()
+  const [updateUserScore] = useUpdateUserScoreMutation()
+  const { data: user } = useGetUserQuery('')
+
+  useEffect(() => {
+    const abortController = new AbortController()
+    const { signal } = abortController
+
+    if (!isFetching && score) {
+      const data = {
+        nickname: user?.display_name || 'Player',
+        date: formatDate(new Date()),
+        rundCodeStudionGameScore: score,
+        country: country || 'Neverland',
+      }
+      updateUserScore({
+        data,
+        signal,
+      }).catch(error => console.info(error))
+      return () => {
+        abortController.abort()
+      }
+    }
+  }, [isFetching, user, coordinates, country, score])
+  return [setScore]
+}
